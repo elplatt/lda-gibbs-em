Fix likelihood calculation.

lda.py
@@ -296,18 +296,15 @@ class LdaModel(object):
         # Virtual word and topic counts
         vmk = self.alpha + stats['nmk']
         vkw = self.eta + stats['nkw']
-        # Multinomial beta of virtual counts
-        mbeta_vmk = multinomial_beta(vmk, 1)[:,np.newaxis]
-        mbeta_vkw = multinomial_beta(vkw, 1)[:,np.newaxis]
         # Expected value of multinomial components
-        nlogtheta = psi(vmk) - psi(vmk.sum(1))[:,np.newaxis]
-        nlogbeta = psi(vkw) - psi(vkw.sum(1))[:,np.newaxis]
+        nlogtheta = (psi(vmk) - psi(vmk.sum(1))[:,np.newaxis])
+        nlogbeta = (psi(vkw) - psi(vkw.sum(1))[:,np.newaxis])
         # Calculate likelihood
         lik = 0
-        lik += (mbeta_vmk * nlogtheta * (vmk - 1)).sum()
-        lik += (mbeta_vkw * nlogbeta * (vkw - 1)).sum()
-        lik -= num_docs * np.log(multinomial_beta(self.alpha))
-        lik -= num_topics * np.log(multinomial_beta(self.eta))
+        lik += (nlogtheta * (vmk - 1)).sum()
+        lik += (nlogbeta * (vkw - 1)).sum()
+        lik -= num_docs * log_multinomial_beta(self.alpha)
+        lik -= num_topics * log_multinomial_beta(self.eta)
         return lik
     
     def topic_conditional(self, m, w, stats):

tests/demoldaem.py
@@ -17,14 +17,14 @@ sys.path.insert(0, os.path.abspath(os.path.join(os.getcwd(), os.pardir)))
 import lda
 
 VOCAB_SIZE = 25
-NUM_TOPICS = 4
+NUM_TOPICS = 10
 NUM_DOCS = 100
-DOC_LENGTH = 10
-NUM_ITER = 50
-GIBBS_BURN = 50
+DOC_LENGTH = 100
+NUM_ITER = 25
+GIBBS_BURN = 0
 GIBBS_LAG = 5
 
-alpha = np.array([0.02, 0.02, 0.02, 0.02])
+alpha = 0.2*np.ones(NUM_TOPICS)
 eta = 0.5*np.ones(VOCAB_SIZE)
 
 id = time.time()
@@ -46,7 +46,16 @@ def main():
     for i in range(NUM_ITER):
         print 'Iteration %d' % i
         model.e_step()
+        lik = model.expected_log_likelihood()
+        print 'ML: %f (%f)' % (lik, lik - last_lik)
+        last_lik = lik
         #model.m_step()
+        model._m_alpha()
+        print "sum alpha = %f " % model.alpha.sum()
+        lik = model.expected_log_likelihood()
+        print 'ML: %f (%f)' % (lik, lik - last_lik)
+        last_lik = lik
+        model._m_eta()
         add_to_img(topic_img, i+1, beta, model.beta())
         lik = model.expected_log_likelihood()
         print 'ML: %f (%f)' % (lik, lik - last_lik)